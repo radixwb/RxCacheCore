dc5a475c21a476239307a459e377184bdaf8b595
radixwb <32069888+radixwb@users.noreply.github.com>
2018-03-28 00:40:48 +0530

    initial code commit

---
+++ Rx.Core.Cache/Models/TagCache.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Rx.Core.Cache.Models
+{
+    public class TagCache
+    {
+        public Type Controller { get; set; }
+
+        public string Etag { get; set; }
+    }
+}
